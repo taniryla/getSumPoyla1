e33dfe9d0a19f70ba404762f461b64841856fb4d
Ryland Taniguchi <ryland0000@gmail.com>
2022-04-12 09:01:48 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// // First, we have to understand the problem.
+// Write a function that takes 2 numbers and returns their sum
+
+// // 1. What is the unknown?
+// the sum of 2 numbers
+
+// // 2. What are the data inputs?
+// a, b
+// // 3. Can you restate the problem in your own words?
+// add a and b together
+// // 4. Can the unknown be determined from the data inputs?
+// yes except large numbers
+// // 5. Is the data inputs sufficient to determine the unknown? Insufficient? Redundant? Contradictory?
+// yes
+// 6. How should I label important pieces of data input that are a part of the problem?
+// // 7. Draw a figure. Introduce suitable notation.
+// this is a basic problem
+// 8. Separate the various parts of the condition. Can you write them down?
+
+function getSum(a, b) {
+  return a + b;
+}
